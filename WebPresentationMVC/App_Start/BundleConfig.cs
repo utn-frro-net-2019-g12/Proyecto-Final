a9ed250901cfb61ccadb0940f6aff7e9d5788176
Vittorio Retrivi <retrovitto@gmail.com>
2019-07-05 11:48:28 -0300

    Added _Layout

--- WebPresentationMVC/App_Start/BundleConfig.cs
+++ WebPresentationMVC/App_Start/BundleConfig.cs
@@ -11,8 +11,14 @@ namespace WebPresentationMVC
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-easing").Include(
+                        "~/Scripts/jquery.easing.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap-bundle").Include(
+                        "~/Scripts/bootstrap.bundle.min.js"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
@@ -22,9 +28,14 @@ namespace WebPresentationMVC
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/sb-2").Include(
+                      "~/Scripts/sb-admin-2.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/sb-2").Include(
+                      "~/Content/sb-admin-2.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/fontawesome").Include(
+                      "~/Content/fontawesome-all.min.css"));
         }
     }
 }
