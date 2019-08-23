be607fd85a50669d6f08b2aedd8a72944e39e60b
= <=>
2019-08-22 21:48:02 -0300

    Add materiaEndpoint and Start MateriaViewModel

--- DesktopPresentationWPF/Models/WpfMateriaModel.cs
+++ DesktopPresentationWPF/Models/WpfMateriaModel.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
 namespace DesktopPresentationWPF.Models {
-    public class WpfMateriaModel {
+    public class WpfMateriaModel : INotifyPropertyChanged {
+        private string _name;
+
         public int Id { get; set; }
 
         public int Year { get; set; }
 
-        public string Name { get; set; }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                CallPropertyChanged(nameof(Name));
+            }
+        }
 
         public bool IsElectiva { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void CallPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
